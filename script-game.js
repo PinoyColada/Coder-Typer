b49b864a7319e56f017c2886620a68df16bdfae4
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-09 21:20:32 -0500

    fix timer bugs

--- script-game.js
+++ script-game.js
@@ -3,12 +3,14 @@ const codeEntered = document.getElementById("input");
 const recycleBtn = document.getElementById("recycle");
 const startBtn = document.getElementById("start");
 const countDownTimer = document.getElementById("timer");
-let time = 60;
-let startTime = 5;
-let reset = false;
+document.getElementById("start").disabled = false;
+let time = 0;
+let startTime = 0;
+let gameWin = false;
+let gameLose = false;
+let timeDuration = null;
 
-let sampleArr = ["win"];
-
+let sampleArr = ["win", "lose", "test"];
 let arrayOfCode = [
     "const array1 = [5, 12, 8, 130, 44]; const isLargeNumber = (element) => element > 13; console.log(array1.findIndex(isLargeNumber));",
     "const array1 = ['a', 'b', 'c', 'd', 'e']; console.log(array1.copyWithin(0, 3, 4)); console.log(array1.copyWithin(1, 3));",
@@ -16,57 +18,19 @@ let arrayOfCode = [
     "const array1 = [1, 2, 3]; console.log(array1.includes(2)); const pets = ['cat', 'dog', 'bat']; console.log(pets.includes('cat'));"
 ]
 
-codeEntered.addEventListener('input', () => {
-    let win = true;
-    const arrayCode = codeShown.querySelectorAll('span');
-    const arraySplit = codeEntered.value.split('');
-    arrayCode.forEach((letterSpan, index) => {
-        const letter = arraySplit[index];
-        if (letter === letterSpan.innerHTML) {
-            letterSpan.classList.add('right');
-            letterSpan.classList.remove('wrong');
-        } else if (letter == null){
-            letterSpan.classList.remove('right');
-            letterSpan.classList.remove('wrong');
-            win = false;
-        } else if (letter === ">" || letter === "<"){
-            if (letter === letterSpan.innerText){
-                letterSpan.classList.add('right');
-                letterSpan.classList.remove('wrong');
-            } else {
-                letterSpan.classList.add('wrong');
-                letterSpan.classList.remove('right');
-                win = false;
-            }
-        } else {
-            if (letterSpan.innerHTML === " "){
-                letterSpan.classList.add('right');
-                letterSpan.classList.remove('wrong');
-            } else {
-                letterSpan.classList.add('wrong');
-                letterSpan.classList.remove('right');
-                win = false;
-            }
-        }
-    });
-
-    if (win) {
-        alert("You completed the game succesfully!");
-
-    }
-});
-
 recycleBtn.addEventListener('click', () => {
     countDownTimer.innerHTML = '';
-    reset = true;
-    time = 60;
-    startTime = 5;
     document.getElementById("input").disabled = true;
+    document.getElementById("start").disabled = false;
     detectCode();
 });
 
 startBtn.addEventListener('click', () => {
-    reset = false;
+    document.getElementById("recycle").disabled = true;
+    time = 0;
+    startTime = 0;
+    gameWin = false;
+    gameLose = false;
     gameStart();
 });
 
@@ -74,7 +38,7 @@ startBtn.addEventListener('click', () => {
 // For each letter, a span is being created containing that letter and is appended to
 // the html of the element id "words"
 function detectCode() {
-    document.getElementById("words").innerHTML = sampleArr[0];
+    document.getElementById("words").innerHTML = sampleArr[Math.floor(Math.random() * sampleArr.length)];
     // document.getElementById("words").innerHTML = arrayOfCode[Math.floor(Math.random() * arrayOfCode.length)];
     const codeToType = document.getElementById("words").innerHTML;
     codeShown.innerHTML = '';
@@ -95,22 +59,25 @@ function detectCode() {
 }
 
 function gameStart() {
-    let timeDuration = setInterval(startTimer, 1000);
+    timeDuration = setInterval(startTimer, 1000);
+    document.getElementById("start").disabled = true;
+    startTime = 5;
 
     function startTimer() {
         let seconds = startTime % 60;
-        if (startTime >= 0 && reset === false) {
+        if (startTime >= 0 && gameWin === false && gameLose === false) {
             seconds = '0' + seconds;
             countDownTimer.innerHTML = `Game Starting in: ${seconds}`;
             startTime--;
-        } else if (reset === true || gameWin === true || gameFailed === true) {
-            clearInterval(startTimer);
+        } else if (gameWin === true || gameLose === true){
+            clearInterval(timeDuration);
         }
 
         if (countDownTimer.innerHTML === 'Game Starting in: 00') {
-            clearInterval(startTimer);
+            clearInterval(timeDuration);
             countDownTimer.innerHTML = '';
             document.getElementById("input").disabled = false;
+            time = 60;
             timeDuration = setInterval(gameTimer, 1000);
         }
     }
@@ -118,16 +85,61 @@ function gameStart() {
     function gameTimer() {
         let minutes = Math.floor(time/60);
         let seconds = time % 60;
-        if (time >= 0 && reset === false) {
+        if (time >= 0 && gameWin === false && gameLose === false) {
             if (seconds < 10) {
                 seconds = '0' + seconds;
             }
             countDownTimer.innerHTML = `Timer: ${minutes}:${seconds}`;
             time--;
-        } else if (reset === true || gameWin === true || gameFailed === true){
-            clearInterval(gameTimer);
+        } else if (gameWin === true || gameLose === true){
+            clearInterval(timeDuration);
         }
     }
+
+    codeEntered.addEventListener('input', () => {
+        let win = true;
+        const arrayCode = codeShown.querySelectorAll('span');
+        const arraySplit = codeEntered.value.split('');
+        arrayCode.forEach((letterSpan, index) => {
+            const letter = arraySplit[index];
+            if (letter === letterSpan.innerHTML) {
+                letterSpan.classList.add('right');
+                letterSpan.classList.remove('wrong');
+            } else if (letter == null){
+                letterSpan.classList.remove('right');
+                letterSpan.classList.remove('wrong');
+                win = false;
+            } else if (letter === ">" || letter === "<"){
+                if (letter === letterSpan.innerText){
+                    letterSpan.classList.add('right');
+                    letterSpan.classList.remove('wrong');
+                } else {
+                    letterSpan.classList.add('wrong');
+                    letterSpan.classList.remove('right');
+                    win = false;
+                }
+            } else {
+                if (letterSpan.innerHTML === " "){
+                    letterSpan.classList.add('right');
+                    letterSpan.classList.remove('wrong');
+                } else {
+                    letterSpan.classList.add('wrong');
+                    letterSpan.classList.remove('right');
+                    win = false;
+                }
+            }
+        });
+    
+        if (win) {
+            alert("You completed the game succesfully!");
+            document.getElementById("input").disabled = true;
+            document.getElementById("recycle").disabled = false;
+            gameWin = true;
+        }
+    });
+    
+
+    
 }
 
 detectCode();
